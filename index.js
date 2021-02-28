d7fb03a1331243111f515a5dc2956a6511a191a5
Suprotim Mukherjee <suprotim6@gmail.com>
2021-02-28 23:05:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+const { executionAsyncResource } = require('async_hooks');
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+ 
+const { YTSearcher } = require('ytsearcher');
+ 
+const searcher = new YTSearcher({
+    key: "Youtube key here",
+    revealed: true
+});
+ 
+const client = new Discord.Client();
+ 
+const queue = new Map();
+
+
+const token = 'ODE1NTI3NDAyODQyMjI2NzEw.YDttFA.C7OImRIapzQpa8AwiOGLgzPlQyU';
+client.login(token);
+ 
+
+client.once('ready', () => {
+    console.log('XeNDeR Bot is running');
+});
+ 
+client.on("message", async(message) => {
+    const prefix = '!';
+ 
+    const serverQueue = queue.get(message.guild.id);
+ 
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase();
+ 
+    switch(command){
+        case 'play':
+            execute(message, serverQueue);
+            break;
+        case 'stop':
+            stop(message, serverQueue);
+            break;
+        case 'skip':
+            skip(message, serverQueue);
+            break;
+    }
+ 
+    async function execute(message, serverQueue){
+        let vc = message.member.voice.channel;
+        if(!vc){
+            return message.channel.send("Please join a voice chat first");
+        }else{
+            let result = await searcher.search(args.join(" "), { type: "video" })
+            const songInfo = await ytdl.getInfo(result.first.url)
+ 
+            let song = {
+                title: songInfo.videoDetails.title,
+                url: songInfo.videoDetails.video_url
+            };
+ 
+            if(!serverQueue){
+                const queueConstructor = {
+                    txtChannel: message.channel,
+                    vChannel: vc,
+                    connection: null,
+                    songs: [],
+                    volume: 10,
+                    playing: true
+                };
+                queue.set(message.guild.id, queueConstructor);
+ 
+                queueConstructor.songs.push(song);
+ 
+                try{
+                    let connection = await vc.join();
+                    queueConstructor.connection = connection;
+                    play(message.guild, queueConstructor.songs[0]);
+                }catch (err){
+                    console.error(err);
+                    queue.delete(message.guild.id);
+                    return message.channel.send(`Unable to join the voice chat ${err}`)
+                }
+            }else{
+                serverQueue.songs.push(song);
+                return message.channel.send(`The song has been added ${song.url}`);
+            }
+        }
+    }
+    function play(guild, song){
+        const serverQueue = queue.get(guild.id);
+        if(!song){
+            serverQueue.vChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+        const dispatcher = serverQueue.connection
+            .play(ytdl(song.url))
+            .on('finish', () =>{
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            })
+            serverQueue.txtChannel.send(`Now playing ${serverQueue.songs[0].url}`)
+    }
+    function stop (message, serverQueue){
+        if(!message.member.voice.channel)
+            return message.channel.send("You need to join the voice chat first!")
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+    }
+    function skip (message, serverQueue){
+        if(!message.member.voice.channel)
+            return message.channel.send("You need to join the voice chat first");
+        if(!serverQueue)
+            return message.channel.send("There is nothing to skip!");
+        serverQueue.connection.dispatcher.end();
+    }
+})
+ 
+        //client.login("Client token here")
+
+        // if(command === 'ping')
+        // {
+        //     message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        //     //message.channel.send('Fucking Bitch Boros Suck my Dick');
+        // }
+        
