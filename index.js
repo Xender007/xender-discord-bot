2f80bb90d0e3f4b37def5635933b4fe22172d6ae
Suprotim Mukherjee <suprotim6@gmail.com>
2021-03-01 23:28:28 +0530

    help embed message design changed

--- index.js
+++ index.js
@@ -133,11 +133,11 @@ client.on("message", async(message) => {
             fields: [
                 {
                     name: 'Song Commands',
-                    value: '`!play` , `!skip` , `!stop`',
+                    value: '`!play`,`!skip`,`!stop`',
                     inline: true,
                 },
                 {
-                    name: 'Test Bot Ping',
+                    name: 'Bot Ping',
                     value: '`!ping`',
                     inline: true,
                 },
@@ -152,7 +152,7 @@ client.on("message", async(message) => {
             // },
             timestamp: new Date(),
             footer: {
-                text: 'Bot Creator -> **__XeNDeR__**',
+                text: '`Bot Creator` -> ***XeNDeR***',
                 icon_url: 'https://scontent.fccu3-1.fna.fbcdn.net/v/t1.15752-9/102319491_282204086297974_8944907308232605696_n.jpg?_nc_cat=104&ccb=3&_nc_sid=ae9488&_nc_ohc=odmekm6DoLMAX_S9qvv&_nc_ht=scontent.fccu3-1.fna&oh=d1fc2cc3b9b4012702ddaef1ec7fe615&oe=60616D63',
             },
         };
