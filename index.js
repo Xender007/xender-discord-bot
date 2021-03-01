abf55a53ff191112460dce49f6ff71ba26eb3848
Suprotim Mukherjee <suprotim6@gmail.com>
2021-03-01 19:37:21 +0530

    help basic embeded message created

--- index.js
+++ index.js
@@ -118,13 +118,58 @@ client.on("message", async(message) => {
             return message.channel.send("There is nothing to skip!");
         serverQueue.connection.dispatcher.end();
     }
-    // function help (message) {
-    //     return message.channel.send('!play song-name -> Type !play then add song name play the song. \n' +
-    //                                 '!stop -> Stop the song. \n' +
-    //                                 '!skip -> Skip the song. \n' +
-    //                                 '!ping -> Get bot ping.');
+    function help (message) {
+        const exampleEmbed = {
+            color: 0x0099ff,
+            title: 'Some title',
+            url: 'https://discord.js.org',
+            author: {
+                name: 'Some name',
+                icon_url: 'https://i.imgur.com/wSTFkRM.png',
+                url: 'https://discord.js.org',
+            },
+            description: 'Some description here',
+            thumbnail: {
+                url: 'https://i.imgur.com/wSTFkRM.png',
+            },
+            fields: [
+                {
+                    name: 'Regular field title',
+                    value: 'Some value here',
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                    inline: false,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+            ],
+            image: {
+                url: 'https://i.imgur.com/wSTFkRM.png',
+            },
+            timestamp: new Date(),
+            footer: {
+                text: 'Some footer text here',
+                icon_url: 'https://i.imgur.com/wSTFkRM.png',
+            },
+        };
         
-    // }
+        return channel.send({ embed: exampleEmbed });
+    }
     function ping (message) {
         return message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
