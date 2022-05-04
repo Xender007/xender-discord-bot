729da0f3ac625d135aceb6b803d57f8fba9bc578
XeNDeR <suprotim6@gmail.com>
2022-05-05 05:12:23 +0530

    slap command improved

--- src/commands/information/slap.js
+++ src/commands/information/slap.js
@@ -1,31 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const {Client, Message, MessageAttachment, MessageEmbed } = require('discord.js');
+const {Canvas} = require('canvacord');
+
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('slap')
         .setDescription('Slap an user')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('user')
-                .setDescription('Info about a user')
-                .addUserOption(option => option.setName('target').setDescription('The user')))
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('server')
-                .setDescription('Info about the server')),
+        .addUserOption(option => option.setName('target').setDescription('The user you selected')),
 
     async execute(interaction) {
 
-        if (interaction.options.getSubcommand() === "user") {
+        const target = interaction.options.getUser('target');
+        const author = interaction.member;
 
-            const target = interaction.options.getUser('target');
-            const author = interaction.member.user.id;
-            await interaction.reply({ content: `<@${author}> Succesfully slapped <@${target.id}>`, ephemeral: true });
-        }
+        const mentionedUserAvatarURL = interaction.options.getMember('target').user.displayAvatarURL({format : "png"});
+        const authorUserAvatarURL = interaction.user.displayAvatarURL({format : "png"});
 
+        const image = await Canvas.slap(mentionedUserAvatarURL, authorUserAvatarURL);
+        const attachment = new MessageAttachment(image, 'image.png')
+
+        await interaction.reply({ content: `<@${author.user.id}> Succesfully slapped <@${target.id}>`, files: [attachment] , ephemeral: false });
 
     },
 
 };
 
-//ODE1NTI3NDAyODQyMjI2NzEw.YDttFA.tNai6knxsDASKF_rnSBZgXGd4gc
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'ready',
 	once: true,
-	async execute(client) {
+	async execute(client) {	
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		client.user.setActivity("/help - get commands.");
 	},
